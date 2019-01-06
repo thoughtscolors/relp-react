d0913811e21c59ee9a6d9e77090e11744bcbbda3
Brady <vanderbannond@gmail.com>
2019-01-05 19:26:45 -0800

    add navbar and change app to class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Restaurants from './components/Restaurants'
+import NavBar from './components/NavBar'
 
-const App = () => (
-  <Router>
-    <div>
-      <Route path="/" exact component={Login} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/restaurants" component={Restaurants} />
-    </div>
-  </Router>
-)
-
-export default App
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavBar/>
+          <Route path="/" exact component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/restaurants" component={Restaurants} />
+        </div>
+      </Router>
+    )
+  }
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+export default class NavBar extends Component {
+
+  state = {
+    isOpen: false
+  }
+
+  toggleOpen = () => {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">RrrrrrELP!</NavbarBrand>
+          <NavbarToggler onClick={this.toggleOpen} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            {this.props.loggedIn ?
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink onClick={this.props.logout}>Logout</NavLink>
+                </NavItem>
+              </Nav>
+            : <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/signup">Signup</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/">Login</NavLink>
+                </NavItem>
+              </Nav>}
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}

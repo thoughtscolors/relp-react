8a9b0d94bf64727a67a355b398003ca75ae5b796
Brady <vanderbannond@gmail.com>
2019-01-07 21:47:40 -0800

    change placeholder to restaurant id

--- src/components/partials/Search.js
+++ src/components/partials/Search.js
@@ -14,7 +14,7 @@ export default class Search extends Component {
       <Form inline onSubmit={(e) => this.props.search(e, searchTerm)}>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label for="search" className="mr-sm-2">Search</Label>
-          <Input type="search" name="search" id="search" placeholder="restaurant name" value={searchTerm}
+          <Input type="search" name="search" id="search" placeholder="restaurant id" value={searchTerm}
             onChange={(e) => this.setState({searchTerm: e.target.value})}
           />
         </FormGroup>
